91648ff69847b3cf688c82c81953e32ce9ba18b5
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2018-07-09 20:56:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(val){
+  drivers.push(val);
+}
+
+function destructivelyPrependDriver(val){
+  drivers.unshift(val);
+}
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
+
+function appendDriver(val){
+  return [...drivers, val]
+}
+
+function prependDriver(val){
+  return [val, ...drivers]
+}
+
+function removeLastDriver(){
+  return drivers.slice(0,-1);
+}
+
+function removeFirstDriver(){
+  return drivers.slice(1);
+}
